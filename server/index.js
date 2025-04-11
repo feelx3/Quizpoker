c184afe9c219194895d71ca949f70cf4413fc489
feelx3 <noahinstgram@gmail.com>
2025-04-11 13:50:23 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const app = express();
 const server = http.createServer(app);
